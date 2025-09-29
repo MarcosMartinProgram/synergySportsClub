a7c905a75079ae3bdb6891b9c64073c660ac89da
Marcos Martin <martinmarcospablo@gmail.com>
2025-09-29 16:00:52 -0300

    Login completo

--- app/src/androidTest/java/com/martinmarcos/synergysportclub/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/martinmarcos/synergysportclub/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.martinmarcos.pruebadam
+package com.martinmarcos.synergysportclub
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/com/martinmarcos/synergysportclub/MenuPrincipalActivity.kt
@@ -0,0 +1,11 @@
+package com.martinmarcos.synergysportclub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_principal)
+    }
+}

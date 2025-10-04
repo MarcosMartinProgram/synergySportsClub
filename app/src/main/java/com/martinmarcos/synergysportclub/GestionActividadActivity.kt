e07d3051a10e416bcbf45472207b66bc6cc545c2
Luis <luis.specterman@fibertel.com.ar>
2025-10-04 12:40:51 -0300

    pantallas finales Gestion - Agregado flecha retroceso  - mensaje en boton grabar y boton salir al menu principal

--- app/src/main/java/com/martinmarcos/synergysportclub/GestionActividadActivity.kt
+++ app/src/main/java/com/martinmarcos/synergysportclub/GestionActividadActivity.kt
@@ -1,13 +1,16 @@
 package com.martinmarcos.synergysportclub
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.content.Intent
+import android.widget.ImageButton
+import android.widget.Toast
 
 class GestionActividadActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_gestion_actividad)
@@ -24,6 +27,25 @@ class GestionActividadActivity : AppCompatActivity() {
             val intent = Intent(this, GestionValorCuotaActivity::class.java)
             startActivity(intent)
         }
+
+        val buttonAtrasMenu = findViewById<ImageButton>(R.id.buttonAtras)
+        buttonAtrasMenu.setOnClickListener {
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSalir = findViewById<Button>(R.id.btnSalir)
+        buttonSalir.setOnClickListener {
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val buttonGrabar = findViewById<Button>(R.id.btnGrabar)
+        buttonGrabar.setOnClickListener {
+            Toast.makeText(this, "GRABAR OK!", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }
--- app/src/main/java/com/martinmarcos/synergysportclub/GestionValorActividadActivity.kt
+++ app/src/main/java/com/martinmarcos/synergysportclub/GestionValorActividadActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.content.Intent
+import android.widget.ImageButton
+import android.widget.Toast
 
 class GestionValorActividadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +18,7 @@ class GestionValorActividadActivity : AppCompatActivity() {
         val btnValorCuota = findViewById<Button>(R.id.btnValorCuota)
 
         btnActividad.setOnClickListener {
-            val intent = Intent(this, GestionValorActividadActivity::class.java)
+            val intent = Intent(this, GestionActividadActivity::class.java)
             startActivity(intent)
         }
 
@@ -24,6 +26,24 @@ class GestionValorActividadActivity : AppCompatActivity() {
             val intent = Intent(this, GestionValorCuotaActivity::class.java)
             startActivity(intent)
         }
+
+        val buttonAtrasMenu = findViewById<ImageButton>(R.id.buttonAtras)
+        buttonAtrasMenu.setOnClickListener {
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSalir = findViewById<Button>(R.id.btnSalir)
+        buttonSalir.setOnClickListener {
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonGrabar = findViewById<Button>(R.id.btnGrabar)
+        buttonGrabar.setOnClickListener {
+            Toast.makeText(this, "GRABAR OK!", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }

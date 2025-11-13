ef0573e491b066b30dc025192799e7861e66921f
Luis <luis.specterman@fibertel.com.ar>
2025-11-12 23:49:28 -0300

    identificacion socio-nosocio al dar de alta

--- app/src/main/java/com/martinmarcos/synergysportclub/ui/RegistroUsuariosActivity.kt
+++ app/src/main/java/com/martinmarcos/synergysportclub/ui/RegistroUsuariosActivity.kt
@@ -14,6 +14,27 @@ class RegistroUsuariosActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_registro_usuarios)
 
+        val switchEsSocio = findViewById<com.google.android.material.switchmaterial.SwitchMaterial>(R.id.switchEsSocio)
+
+        // Establecer el texto inicial basado en si está marcado o no (por si acaso se restaura el estado)
+        if (switchEsSocio.isChecked) {
+            switchEsSocio.text = "Socio"
+        } else {
+            switchEsSocio.text = "No Socio"
+        }
+
+        // Añadir un listener para cambiar el texto cuando el usuario interactúe
+        switchEsSocio.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // El Switch está activado (a la derecha)
+                switchEsSocio.text = "Socio"
+            } else {
+                // El Switch está desactivado (a la izquierda)
+                switchEsSocio.text = "No Socio"
+            }
+        }
+
+
 
         val buttonInscribir = findViewById<Button>(R.id.buttonRecuperar)
         buttonInscribir.setOnClickListener {
